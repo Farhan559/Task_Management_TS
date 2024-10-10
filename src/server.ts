6428e29282128ea783b196828cb927fc2923ba89
Farhan559 <farhansiddiqui559@gmail.com>
2024-10-10 19:27:38 +0500

    feat: add Routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import taskRoutes from './routes/task.routes';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //Routes
-app.use('/api/tasks',);
+app.use('/api/tasks', taskRoutes);
 
 // MongoDB Connection
 
