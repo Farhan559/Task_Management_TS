9cc6439a7ffddd38316167283808669f7c559e58
Farhan559 <farhansiddiqui559@gmail.com>
2024-10-10 18:32:24 +0500

    Project initialized

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks',);
+
+// MongoDB Connection
+
+mongoose.connect(process.env.MONGO_URI ||'',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log(`Connected to MongoDb`);
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log('Error connecting to MongoDb',error);
+})
